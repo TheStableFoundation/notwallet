90b105582c7cccced9af9f6fa832092dcf050566
keypair34 <work@setoelkahfi.se>
2025-07-26 08:57:07 +0200

    Bump versionCode to 4

--- wearOSApp/app/build.gradle.kts
+++ wearOSApp/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "xyz.notwallet.NotWallet"
         minSdk = 30
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
     }
 
