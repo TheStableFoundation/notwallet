7eccf69527a6ab90fe298d1dc81b3b63195437aa
Seto Elkahfi <setoelkahfi@gmail.com>
2025-08-06 19:55:48 +0200

    wip

---
+++ NotWalletWatchApp/NotWalletWatchApp Watch App/NotWalletWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotWalletWatchAppApp.swift
+//  NotWalletWatchApp Watch App
+//
+//  Created by Seto Elkahfi on 2025-08-06.
+//
+
+import SwiftUI
+
+@main
+struct NotWalletWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
